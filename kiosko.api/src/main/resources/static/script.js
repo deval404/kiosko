656fea8263ce813cda22af5714ab096f5d6395bc
valentin <404valem@gmail.com>
2025-10-21 07:51:27 -0300

    url de la api aplicada a los fetch

--- kiosko.api/src/main/resources/static/script.js
+++ kiosko.api/src/main/resources/static/script.js
@@ -6,7 +6,7 @@ let productos = [];
 let carrito = JSON.parse(localStorage.getItem('carrito')) || [];
 let isAdmin = JSON.parse(localStorage.getItem('isAdmin')) || false;
 
-const API_URL = "http://localhost:8080/productos";
+const API_URL = "https://api.render.com/deploy/srv-d3rlrn95pdvs73fqaa90?key=0BmmwAZZ_9c/productos";
 
 // Elementos del DOM
 const gridProductos = document.getElementById('grid-productos');
